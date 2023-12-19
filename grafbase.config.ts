e86e37997ac1253f343bc01511c62f2c71f58932
cedric debailleul <cedric.debailleul62@gmail.com>
2023-12-20 00:19:14 +0100

    :construction: grafbase

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,27 +1,52 @@
-import { config, connector, graph } from '@grafbase/sdk'
+import {config, connector, graph } from '@grafbase/sdk'
 
 const g = graph.Standalone()
 
-const User = g.type('User', {
-  name: g.string().length({min: 2, max: 20}),
-  email: g.string().unique(),
+const pg = connector.Postgres('pg', {
+  url: g.env('DATABASE_URL'),
+})
+
+g.datasource(pg)
+
+const user = g.type('User', {
+  id: g.id(),
+  name: g.string().optional(),
+  email: g.string(),
   avatarUrl: g.url(),
   description: g.string().optional(),
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(),
-  projects: g.relation(() => Project).list().optional(),
 })
 
-const Project = g.type('Project', {
+// @ts-ignore
+pg.model('User', {
+  name: g.string().length({min: 2, max: 100}),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000}).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+}).collection('users')
+.auth((rules: any) => {
+  rules.public().read()
+})
+
+// @ts-ignore
+pg.model('Project', {
   title: g.string().length({min: 3}),
   description: g.string(),
   image: g.url(),
   liveSiteUrl: g.url(),
   githubUrl: g.url(),
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
+  category: g.string(),
+  createdBy: g.ref(user),
+})
+.collection('projects')
+.auth((rules: any) => {
+  rules.public().read()
+  rules.private().create().delete().update()
 })
 
 export default config({
-  graph: g
-  })
+  graph: g,
+})
